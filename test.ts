50b7b4a719d22fde9e8d9088842f4e22c6e1222a
Tom Ball <tballmsft@users.noreply.github.com>
2020-12-04 14:42:30 -0800

    change test

--- test.ts
+++ test.ts
@@ -1,5 +1,8 @@
 let strip = neopixel.create(DigitalPin.P0, 256, NeoPixelMode.RGB)
 pins.digitalWritePin(DigitalPin.P1, 1)
 strip.setBrightness(30)
+// higher-level API (enum)
 EPXDisplay.play(strip, EPXAnimations.ColorSpin)
-strip.show()
+// lower-level API (buffer)
+EPXDisplay.writeAnimation(strip, EPXDisplay.weather)
+strip.show();
